0d4e2b9f2fa8e0fceb78785eea333c519348f4ad
Scoozy <s.safanovich@gmail.com>
2022-12-28 20:34:01 -0800

    haha

--- Team1515/ournewteam.js
+++ Team1515/ournewteam.js
@@ -1,18 +1,28 @@
 let buttons = document.querySelectorAll(".expandbtn")
+let personCard = document.querySelectorAll(".personCard")
 
+   
 buttons.forEach(
+    
     (button) => {
         button.addEventListener("click", (event) => {
+            
             let sibling = event.target.previousElementSibling
+            
             if (sibling.classList.contains("expand")) {
+                
                 event.target.innerHTML = "Read More"
                 sibling.classList.remove("expand")
             }
             else {
+                
                 event.target.innerHTML = "Read Less"
                 sibling.classList.add("expand")
-            }
 
+            }
+            
+            
         })
     }
-)
+    
+) 
